f5bc5c79f0b48460f10feaf7a9dfc4a9b2d28e18
soufien <aloui19940724@gmail.com>
2026-08-16 19:19:25 +0100

    Wrap app with next-auth SessionProvider so useSession works and prevents prerender error

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
 import Head from 'next/head';
 import '../styles/globals.css';
+import { SessionProvider } from 'next-auth/react';
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -12,6 +13,6 @@ export default function App({ Component, pageProps }) {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
       </Head>
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
