93667df3a9bb32c3d979cbee97ea367d83a160a2
soufien <aloui19940724@gmail.com>
2026-08-16 19:12:28 +0100

    Fix: remove integrity attribute from fontawesome, ensure font preconnect uses anonymous, and add root index.html redirect to Next app

--- pages/_app.js
+++ pages/_app.js
@@ -6,12 +6,10 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <!-- Google Font Cairo -->
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;600;700;800&display=swap" rel="stylesheet" />
-        <!-- Font Awesome -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-p1CmO1m6lZbK2Yt3e1Y1q0V2k3wKq3q8zGz3i5Yv4v6Y1e9K7Q3Y1G7Y1e9K7Q3Y1G7Y1e9K7Q==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
       </Head>
       <Component {...pageProps} />
     </>
